cd7698d34932d7080ffc11cd4bab83a1ba7e2868
Francisco Tarantuviez <ft@MacBook-Pro-de-Francisco-2.local>
2025-04-13 19:34:21 -0300

    feat: add rate limit

--- src/api/server.ts
+++ src/api/server.ts
@@ -4,15 +4,21 @@ import dotenv from "dotenv";
 
 import { logger } from "../utils/logger";
 import transactionsRouter from "./routes/transactions";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 300, // Limit each IP to 300 requests per windowMs
+});
+
 app.use(cors());
 app.use(express.json());
-
+app.use(limiter);
 app.use("/transactions", transactionsRouter);
 
 // Start server
